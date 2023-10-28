caa47a09cb7b9a6a17ab0af870330801972a783c
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-10-28 23:31:04 +0000

    pedra papel tesoura como funcao

---
+++ pedra_papel_tesoura_funcao.py
@@ -0,0 +1,55 @@
+import random
+ 
+def condicoes_iniciais():
+    rodada = 1
+    score_usuario = 0
+    score_computador = 0
+    return rodada, score_usuario, score_computador
+
+def jogadas_possiveis():
+    possibilidades = ('pedra', 'papel', 'tesoura')
+    jogada_usuario = input('Digite pedra, papel ou tesoura: ')
+    jogada_computador = random.choice(possibilidades)
+    
+    if jogada_usuario not in possibilidades:
+        print('Jogada inválida!')
+
+    return possibilidades, jogada_usuario, jogada_computador
+
+rodada, score_usuario, score_computador = condicoes_iniciais()
+
+while True:
+
+    possibilidades, jogada_usuario, jogada_computador = jogadas_possiveis()
+
+    print(f'Score >> {score_usuario}:{score_computador}')
+    print(f'Rodada: {rodada}')  
+
+    if score_usuario == 3 or score_computador == 3:
+        print(f'A partida acabou. O score ficou: {score_usuario}:{score_computador} e o número de rodadas foram {rodada}')
+        break 
+
+    # Caso em que empata
+    if jogada_usuario == jogada_computador: 
+        print('Empate')
+        rodada += 1
+        continue 
+
+    # Caso em que o jogador vence
+    if (jogada_usuario == 'pedra' and jogada_computador == 'tesoura') or \
+        (jogada_usuario == 'papel' and jogada_computador == 'pedra') or \
+        (jogada_usuario == 'tesoura' and jogada_computador == 'papel'):
+        print('Você venceu!')
+        rodada += 1 
+        score_usuario += 1
+        continue
+           
+
+    # Caso em que o computador vence
+    if (jogada_computador == 'pedra' and jogada_usuario == 'tesoura') or \
+        (jogada_computador == 'papel' and jogada_usuario == 'pedra') or \
+        (jogada_computador == 'tesoura' and jogada_usuario == 'papel'):
+        print('O computador venceu!')
+        rodada += 1
+        score_computador += 1
+        continue
