f5356d214a479cd8c989f45da3c694a4544a5205
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-10-28 15:21:56 +0000

    introduzindo o yield

---
+++ generator.py
@@ -0,0 +1,12 @@
+# entendo o generator e a diferença de yield e return
+
+tupla = (1,2,3,4,5)
+
+def generator(tupla):
+    for i in tupla:
+        yield i**2 #quando o loop é retomado, começa-se do yield
+
+g = generator(tupla)
+
+for i in range(5):
+    print(next(g))
