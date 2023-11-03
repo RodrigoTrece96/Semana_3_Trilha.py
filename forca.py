abedbebaa1462275bd8707df991b49d09680e132
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-11-03 01:20:02 +0000

    Forca no Python

--- forca.py
+++ forca.py
@@ -3,35 +3,50 @@
 import random
 
 def condicoes_iniciais(): # Desempacotado
-    possibilidades = ('loki', 'valk', 'engenharia', 'biotecnologia', 'pedagogia', 'guitarra')
+
+    possibilidades = ('processos', 'computador', 'engenharia', 'biotecnologia', 'pedagogia', 'guitarra', 'industria', 'quimica', 
+    'cachorro', 'gato', 'mate', 'sushi')
+
     palavra = random.choice(possibilidades)
-    return palavra
+    rodada = 0
+    return palavra, rodada
 
-palavra = condicoes_iniciais()
-
-def tamanho_palavra():
-    tamanho = '_' * len(palavra) 
-    return tamanho
-
+palavra, rodada = condicoes_iniciais()
 
 def andamento_jogo():
-    rodada = 0
-    return rodada
+    tamanho = '_' * len(palavra) 
+    letras_corretas = [] # Aqui serao adicionadas as letras corretas  
+    return tamanho, letras_corretas
 
+tamanho, letras_corretas = andamento_jogo()
 
+print(f'O tamanho da palavra é: {tamanho}')
 
 while True:
 
-    tamanho = tamanho_palavra()
-    rodada = andamento_jogo()
+    rodada += 1 
 
-    print(palavra)
-    print(f'Esse é o número de letras da palavra: {tamanho}')
     print(f'Rodada: {rodada}')
     jogada_usuario = input('Digite uma letra: ').lower()
 
-    if rodada > 8:
-        print('As tentavivas acabaram. Fim de jogo!')
-        break 
+    if jogada_usuario in palavra:
+        letras_corretas.append(jogada_usuario)
+    
+    palavra_usuario = ""
 
-# Falta descobrir como trocar o '_' pela letra! 
+    for letra in palavra:
+        if letra in letras_corretas:
+            palavra_usuario += letra # Aqui tem que ser letra, caso seja jogada_usuario, a cada iteração a letra que ele escolheu vai para a string!!!
+        
+        else:
+            palavra_usuario += '_'
+
+    print(f'Seu progresso está assim: {palavra_usuario}')
+    
+    if palavra_usuario == palavra:
+        print('Parabéns, você venceu!')
+        break
+
+    if rodada == 20: # Final da partida
+        print(f'As tentativas acabaram! A palavra correta era {palavra}. Foi uma boa rodada!')
+        break 
