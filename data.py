7f5167858c879c7785699bb0598455ea1ccb4585
Rodrigo Trece <145849765+RodrigoTrece96@users.noreply.github.com>
2023-11-08 12:49:27 +0000

    iniciando no pandas

--- data.py
+++ data.py
@@ -1,2 +1,20 @@
-import pandas as pd 
+# Nota: antes estava dando erro porque o nome do arquivo era o mesmo nome da biblioteca
+# O erro era "circular import" 
+
+import pandas as pd
+
+data = pd.read_csv('teste.csv')
+
+rows = data.head(3) # por default: 5
+rows2 = data.tail(2) # por default: 5
+
+#print(data)
+
+counting = data.count('index') #conta quantos rows tem o DataFrame
+counting2 = data.count('columns') #conts quantas colunas tem o DataFrame
+print(counting, counting2)
+
+
+
+
 
